b75df8623dece1dc8b400a514cc7d2e8fcffc311
bumsol2 <48481431+bumsol2@users.noreply.github.com>
2022-07-08 10:12:49 +0900

    학원 교육과정 파워쉘로 만들기
    OU 생성 => 유저생성 => OU 이동 파워쉘 삽질

---
+++ 계정만들기.ps1
@@ -0,0 +1,24 @@
+﻿#AD cmdlet 실행을 위한 Active Directory 모듈 가져오기
+Import-Module activedirectory  
+
+# 1.원더걸스 OU 생성
+New-ADOrganizationalUnit -Name "원더걸스" -Path "DC=sol,DC=COM" -ProtectedFromAccidentalDeletion $False
+
+# 2. 원더걸스 OU 안에 사용자 생성
+New-ADUser -Name “소희"  -Surname "소희" -SamAccountName "wonder1" -UserPrincipalName "wonder1@itbank.vm" -path "OU=원더걸스,DC=sol,DC=com" -Accountpassword (Read-Host -AsSecureString "소희계정패스워드입력해") -enabled $True
+
+New-ADUser -Name “선예"  -Surname "선예" -SamAccountName "wonder2" -UserPrincipalName "wonder2@itbank.vm" -path "OU=원더걸스,DC=sol,DC=com" -Accountpassword (Read-Host -AsSecureString "선예패스워드입력해") -enabled $True
+
+# 3. 소녀시대 OU 생성
+New-ADOrganizationalUnit -Name "소녀시대" -Path "DC=sol,DC=COM" -ProtectedFromAccidentalDeletion $False
+
+#4. 소녀시대 ou 사용자 생성
+New-ADUser -Name “태연"  -Surname "태연" -SamAccountName "wonder3" -UserPrincipalName "girl1@itbank.vm" -path "OU=소녀시대,DC=sol,DC=com" -Accountpassword (Read-Host -AsSecureString "태연계정패스워드입력해") -enabled $True
+New-ADUser -Name “윤아"  -Surname "윤아" -SamAccountName "wonder4" -UserPrincipalName "girl2@itbank.vm" -path "OU=소녀시대,DC=sol,DC=com" -Accountpassword (Read-Host -AsSecureString "윤아계정패스워드입력해") -enabled $True
+
+# 5. 수강생 ou 생성대
+New-ADOrganizationalUnit -Name "수강생" -Path "DC=sol,DC=COM" -ProtectedFromAccidentalDeletion $False
+
+# 6. 원더걸스OU와 소녀시대OU를 부서-수강생 OU로 이동
+Move-ADObject -Identity "OU=원더걸스,DC=sol,DC=com" -TargetPath "OU=수강생,DC=sol,DC=com"
+Move-ADObject -Identity "OU=소녀시대,DC=sol,DC=com" -TargetPath "OU=수강생,DC=sol,DC=com"
---
+++ 계정만들기2.ps1
@@ -0,0 +1,16 @@
+﻿#AD cmdlet 실행을 위한 Active Directory 모듈 가져오기
+Import-Module activedirectory
+
+#1. 핑클 OU를 생성하시오
+New-ADOrganizationalUnit -Name "핑클" -Path "DC=sol,DC=COM" -ProtectedFromAccidentalDeletion $False
+
+#2번 3번까지  핑클 OU안에 4명의 유저를 생성하시오. SAM ID와 UPN만 가지고 생성하고 암호 입력 후 계정 활성화까지
+New-ADUser -Name “이효리"  -SamAccountName "finkle1" -UserPrincipalName "f1@itbank.vm" -path "OU=핑클,DC=sol,DC=com" -Accountpassword (Read-Host -AsSecureString "이효리계정패스워드입력해") -enabled $True
+New-ADUser -Name “옥주현"  -SamAccountName "finkle2" -UserPrincipalName "f2@itbank.vm" -path "OU=핑클,DC=sol,DC=com" -Accountpassword (Read-Host -AsSecureString "옥주현계정패스워드입력해") -enabled $True
+New-ADUser -Name “성유리"  -SamAccountName "finkle3" -UserPrincipalName "f3@itbank.vm" -path "OU=핑클,DC=sol,DC=com" -Accountpassword (Read-Host -AsSecureString "성유리계정패스워드입력해") -enabled $True
+New-ADUser -Name “이진"  -SamAccountName "finkle4" -UserPrincipalName "f4@itbank.vm" -path "OU=핑클,DC=sol,DC=com" -Accountpassword (Read-Host -AsSecureString "이진계정패스워드입력해") -enabled $True
+
+#연예인 OU를 만들고, 소녀시대 OU와 핑클 OU를 이동하시오.
+New-ADOrganizationalUnit -Name "연예인" -Path "DC=sol,DC=COM" -ProtectedFromAccidentalDeletion $False
+Move-ADObject -Identity "OU=소녀시대,OU=수강생,DC=sol,DC=com" -TargetPath "OU=연예인,DC=sol,DC=com"
+Move-ADObject -Identity "OU=핑클,DC=sol,DC=com" -TargetPath "OU=연예인,DC=sol,DC=com"
